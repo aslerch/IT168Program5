a104e4ca152ca3653f1ca8d9f176387ac889639e
aslerch <88261471+aslerch@users.noreply.github.com>
2021-08-02 12:36:18 -0800

    added display menu and started to write menu loop

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     private static String stars = "*********************************************";
     private static String welcomeSign = stars
             + "\nWelcome to the university Registration System\n"
             + stars;
-    private static String menu = "Please select one of the options below";
+    private static String menu = """
+            Please select one of the options below:
+            1. Add course to the system
+            2. Add student to the system
+            3. Register a student to a course
+            4. Display student information
+            5. Exit the system""";
+    private static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String [] args) {
 
-        System.out.println(welcomeSign);
+        // objects needed later
+        int addCourse = 1, addStudent = 2, registerStudent = 3, displayStudent = 4, exit = 5;
+
+        // main
+        System.out.println(welcomeSign + "\n" + menu);
+        while ( keyboard.nextInt() != exit) {
+
+        }
+        System.out.println("Thank you for using the university Registration System");
 
     }
 
