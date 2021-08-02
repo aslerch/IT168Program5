a18a5a0080857d08f666e3ae9e89fd4f851bfee6
aslerch <88261471+aslerch@users.noreply.github.com>
2021-08-02 12:22:50 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private static String stars = "*********************************************";
+    private static String welcomeSign = stars
+            + "\nWelcome to the university Registration System\n"
+            + stars;
+    private static String menu = "Please select one of the options below";
+
+    public static void main(String [] args) {
+
+        System.out.println(welcomeSign);
+
+    }
+
+
+
+}
